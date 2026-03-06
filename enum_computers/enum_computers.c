14c7310258ef46901dddd6f75b3557837ff79712
lineeralgebra <94987429+lineeralgebra@users.noreply.github.com>
2026-03-06 23:36:55 +0000

    Add files via upload

---
+++ enum_computers/enum_computers.c
@@ -0,0 +1,131 @@
+#include <windows.h>
+#include <winldap.h>
+#include "beacon.h"
+
+/* Wldap32.dll - Wide (W) versions for auto-discovery */
+DECLSPEC_IMPORT LDAP* WINAPI WLDAP32$ldap_initW(PWSTR, ULONG);
+DECLSPEC_IMPORT ULONG WINAPI WLDAP32$ldap_set_optionW(LDAP*, int, PVOID);
+DECLSPEC_IMPORT ULONG WINAPI WLDAP32$ldap_connect(LDAP*, struct l_timeval*);
+DECLSPEC_IMPORT ULONG WINAPI WLDAP32$ldap_bind_sW(LDAP*, PWSTR, PWSTR, ULONG);
+DECLSPEC_IMPORT ULONG WINAPI WLDAP32$ldap_search_ext_sW(LDAP*, PWSTR, ULONG, PWSTR, PWSTR*, ULONG, PLDAPControlW*, PLDAPControlW*, struct l_timeval*, ULONG, LDAPMessage**);
+DECLSPEC_IMPORT ULONG WINAPI WLDAP32$ldap_count_entries(LDAP*, LDAPMessage*);
+DECLSPEC_IMPORT LDAPMessage* WINAPI WLDAP32$ldap_first_entry(LDAP*, LDAPMessage*);
+DECLSPEC_IMPORT LDAPMessage* WINAPI WLDAP32$ldap_next_entry(LDAP*, LDAPMessage*);
+DECLSPEC_IMPORT PWSTR* WINAPI WLDAP32$ldap_get_valuesW(LDAP*, LDAPMessage*, PWSTR);
+DECLSPEC_IMPORT ULONG WINAPI WLDAP32$ldap_value_freeW(PWSTR*);
+DECLSPEC_IMPORT ULONG WINAPI WLDAP32$ldap_msgfree(LDAPMessage*);
+DECLSPEC_IMPORT ULONG WINAPI WLDAP32$ldap_unbind(LDAP*);
+
+/* MSVCRT.dll */
+DECLSPEC_IMPORT size_t WINAPI MSVCRT$strlen(const char*);
+
+void go(char* args, int len) {
+    datap parser;
+    BeaconDataParse(&parser, args, len);
+
+    /* optional domain argument */
+    char* domain_a = BeaconDataExtract(&parser, NULL);
+    wchar_t domain_w[256];
+    wchar_t* pDomain = NULL;
+
+    if (domain_a && MSVCRT$strlen(domain_a) > 0) {
+        if (toWideChar(domain_a, domain_w, 256)) {
+            pDomain = domain_w;
+        }
+    }
+
+    LDAP* ld = WLDAP32$ldap_initW(pDomain, 389);
+    if (ld == NULL) {
+        BeaconPrintf(CALLBACK_ERROR, "ldap_init failed");
+        return;
+    }
+
+    ULONG version = LDAP_VERSION3;
+    WLDAP32$ldap_set_optionW(ld, LDAP_OPT_PROTOCOL_VERSION, &version);
+
+    ULONG res = WLDAP32$ldap_connect(ld, NULL);
+    if (res != LDAP_SUCCESS) {
+        BeaconPrintf(CALLBACK_ERROR, "ldap_connect failed: 0x%x", res);
+        WLDAP32$ldap_unbind(ld);
+        return;
+    }
+
+    res = WLDAP32$ldap_bind_sW(ld, NULL, NULL, LDAP_AUTH_NEGOTIATE);
+    if (res != LDAP_SUCCESS) {
+        BeaconPrintf(CALLBACK_ERROR, "ldap_bind_s failed: 0x%x", res);
+        WLDAP32$ldap_unbind(ld);
+        return;
+    }
+
+    /* Query RootDSE for defaultNamingContext (auto-discover base DN) */
+    LDAPMessage* rootResult = NULL;
+    PWSTR rootAttrs[] = { L"defaultNamingContext", NULL };
+    PWSTR baseDN = NULL;
+
+    res = WLDAP32$ldap_search_ext_sW(ld, L"", LDAP_SCOPE_BASE, L"(objectClass=*)", rootAttrs, 0, NULL, NULL, NULL, 0, &rootResult);
+    if (res == LDAP_SUCCESS) {
+        LDAPMessage* entry = WLDAP32$ldap_first_entry(ld, rootResult);
+        if (entry) {
+            PWSTR* values = WLDAP32$ldap_get_valuesW(ld, entry, L"defaultNamingContext");
+            if (values) {
+                baseDN = values[0];
+            }
+        }
+    } else {
+        BeaconPrintf(CALLBACK_ERROR, "Failed to query RootDSE: 0x%x", res);
+        if (rootResult) WLDAP32$ldap_msgfree(rootResult);
+        WLDAP32$ldap_unbind(ld);
+        return;
+    }
+
+    /* Search for all computers */
+    LDAPMessage* searchResult = NULL;
+    PWSTR attrs[] = { L"sAMAccountName", L"dNSHostName", L"operatingSystem", NULL };
+
+    res = WLDAP32$ldap_search_ext_sW(ld, baseDN, LDAP_SCOPE_SUBTREE, L"(objectClass=computer)", attrs, 0, NULL, NULL, NULL, 0, &searchResult);
+
+    if (res != LDAP_SUCCESS) {
+        BeaconPrintf(CALLBACK_ERROR, "ldap_search failed: 0x%x", res);
+        if (rootResult) WLDAP32$ldap_msgfree(rootResult);
+        WLDAP32$ldap_unbind(ld);
+        return;
+    }
+
+    /* Use formatp to buffer all output into a single chunk */
+    formatp buffer;
+    BeaconFormatAlloc(&buffer, 16 * 1024);
+
+    BeaconFormatPrintf(&buffer, "[+] Using Base DN: %ls\n\n", baseDN ? baseDN : L"N/A");
+
+    DWORD count = 0;
+    LDAPMessage* entry = WLDAP32$ldap_first_entry(ld, searchResult);
+    while (entry != NULL) {
+        PWSTR* name = WLDAP32$ldap_get_valuesW(ld, entry, L"sAMAccountName");
+        PWSTR* dns = WLDAP32$ldap_get_valuesW(ld, entry, L"dNSHostName");
+        PWSTR* os = WLDAP32$ldap_get_valuesW(ld, entry, L"operatingSystem");
+
+        BeaconFormatPrintf(&buffer, "Computer: %ls (DNS: %ls, OS: %ls)\n",
+            name ? name[0] : L"N/A",
+            dns ? dns[0] : L"N/A",
+            os ? os[0] : L"N/A");
+
+        if (name) WLDAP32$ldap_value_freeW(name);
+        if (dns) WLDAP32$ldap_value_freeW(dns);
+        if (os) WLDAP32$ldap_value_freeW(os);
+
+        entry = WLDAP32$ldap_next_entry(ld, entry);
+        count++;
+    }
+
+    BeaconFormatPrintf(&buffer, "\nTotal computers found: %d\n", count);
+
+    /* Send everything as one output block */
+    int outLen = 0;
+    char* outData = BeaconFormatToString(&buffer, &outLen);
+    BeaconOutput(CALLBACK_OUTPUT, outData, outLen);
+    BeaconFormatFree(&buffer);
+
+    if (searchResult) WLDAP32$ldap_msgfree(searchResult);
+    if (rootResult) WLDAP32$ldap_msgfree(rootResult);
+    WLDAP32$ldap_unbind(ld);
+}
